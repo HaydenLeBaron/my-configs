efcff3b7cb79c65b737b4b0928d35d35df9429e5
Hayden LeBaron <me@haydenlebaron.com>
2022-02-15 19:24:50 -0700

    Update setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 brew install neovim
 brew install obsidian
 brew install alfred
+brew install git
+brew install git-credential-manager
 git config --global user.name "Hayden LeBaron"
 git config --global user.email "me@haydenlebaron.com"
 #TODO: clone .zshrc
