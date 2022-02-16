983348b09f217dc5f27b5b6141d69b99e34ee892
Hayden LeBaron <me@haydenlebaron.com>
2022-02-15 19:16:56 -0700

    Initial commit: add some brew & git initalization

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+brew install neovim
+brew install obsidian
+brew install alfred
+git config --global user.name "Hayden LeBaron"
+git config --global user.email "me@haydenlebaron.com"
+#TODO: clone .zshrc
+
