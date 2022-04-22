99bcf24d0a934dfe15eb5cb4aa64f36f199ff916
HaydenLeBaron <me@haydenlebaron.com>
2022-04-22 13:30:20 -0700

    update

--- update-configs.sh
+++ update-configs.sh
@@ -7,7 +7,6 @@
 
 echo "Removing old configs..."
 rm config/.zshrc
-rm config/.gitconfig
 rm -rf config/.doom.d
 rm -rf config/.vim
 rm -rf config/.gitkraken
@@ -15,7 +14,6 @@ rm config/nvim/init.vim
 
 echo "Copying current configs to my-scripts repo..."
 cp    ~/.zshrc             config/.zshrc
-cp    ~/.gitconfig         config/.gitconfig
 cp -r ~/.doom.d            config/.doom.d
 cp -r ~/.vim               config/.vim
 cp -r ~/.gitkraken         config/.gitkraken
